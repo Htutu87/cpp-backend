f8d24b595f152151dd209cbf1e45c07d42426e14
Artur Amaral <artur_amaral@poli.ufrj.br>
2022-03-21 23:40:36 -0300

    Timer 1 example.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 
 #include <iostream>
-#include "asio.hpp"
+#include <asio.hpp>
+#include <date_time/posix_time/posix_time.hpp>
 
 int main() 
 {
     std::cout << "Hello, world!" << '\n'; 
     std::cout << "Asio installed!" << '\n'; 
+    std::cout << "Timer 1 example!" << '\n'; 
+
+	boost::asio::io_service io;
+	boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
+	t.wait();
+
+    std::cout << "5 seconds have passed!" << "\n\n"; 
+
     return 0;
 }
